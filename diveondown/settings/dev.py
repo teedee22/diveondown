7e975cca499acafd7f5509ac8ecdc1959c8eaea1
Toby <tdudleyoffice@gmail.com>
2019-10-31 11:05:04 -0500

    initialised blank wagtail

---
+++ diveondown/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'al1^jq&vt8n9*+7z%eie&44p!vsx^khu$l)l^gk+-9*ngwd9rm'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
