ba2a37dfa9520f20ca0c88540746832cc8bcf33f
Toby <tdudleyoffice@gmail.com>
2019-11-11 16:30:18 -0500

    fix staticfiles issue, remove js reference

--- diveondown/settings/production.py
+++ diveondown/settings/production.py
@@ -63,3 +63,37 @@ try:
     from .local import *
 except ImportError:
     pass
+"""
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'diveondown.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+"""
