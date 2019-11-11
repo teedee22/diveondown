d45e91a83da3a6494e9ff86795ba9327b5a1315d
Toby <tdudleyoffice@gmail.com>
2019-11-11 11:34:30 -0500

    remove media from base.py

--- diveondown/settings/dev.py
+++ diveondown/settings/dev.py
@@ -16,7 +16,8 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 try:
     from .local import *
 except ImportError:
